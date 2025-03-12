6a362299cf6bf375cf28f72e0c03be114094261a
Tojimatov Abdukarim <walterbriendeveloper@gmail.com>
2025-03-12 14:23:03 +0500

    feat: added discounts

--- src/main.jsx
+++ src/main.jsx
@@ -7,16 +7,27 @@ import GridBackground from "./components/ui/GridBackground.jsx";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 // Determine the GraphQL endpoint based on the current hostname at runtime
+// const isLocalhost =
+//   window.location.hostname === "localhost" ||
+//   window.location.hostname === "127.0.0.1";
+// const serverIP = "92.112.180.30"; // Your server IP
+
+// // Use the current hostname for the GraphQL endpoint to handle custom domains
+// // Use same protocol as the page to avoid mixed content errors
+// const protocol = window.location.protocol; // Will be 'http:' or 'https:'
+// const graphqlURI = isLocalhost
+//   ? "http://localhost:4000/graphql"
+//   : `${protocol}//${window.location.hostname}/graphql`;
+
 const isLocalhost =
   window.location.hostname === "localhost" ||
   window.location.hostname === "127.0.0.1";
-const serverIP = "92.112.180.30"; // Your server IP
-
-// Use the current hostname for the GraphQL endpoint to handle custom domains
-// Use same protocol as the page to avoid mixed content errors
-const protocol = window.location.protocol; // Will be 'http:' or 'https:'
+const isIP = window.location.hostname === "92.112.180.30";
+const protocol = window.location.protocol;
 const graphqlURI = isLocalhost
   ? "http://localhost:4000/graphql"
+  : isIP
+  ? "http://92.112.180.30:4000/graphql"
   : `${protocol}//${window.location.hostname}/graphql`;
 
 // console.log(`Current hostname: ${window.location.hostname}`);
